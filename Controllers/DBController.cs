a05c366dcf5827246fa5728f6823a833cd03a862
tuandzhp <tuan85380@st.vimaru.edu.vn>
2023-05-24 16:31:03 +0700

    Add controllers

---
+++ Controllers/DBController.cs
@@ -0,0 +1,77 @@
+﻿using aps_viewer_db_properties.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using System;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DBController : ControllerBase
+{
+  public readonly IHubContext<DBHub> _dbHub;
+  private readonly ILogger<DBController> _logger;
+  private readonly DBService _dbService;
+
+  public DBController(ILogger<DBController> logger, DBService dbService, IHubContext<DBHub> dbHub)
+  {
+    _dbHub = dbHub;
+    GC.KeepAlive(_dbHub);
+    _logger = logger;
+    _dbService = dbService;
+  }
+
+  [HttpPost("dbconnector")]
+  public object PostDBData([FromBody] DBUpdate dBUpdate)
+  {
+
+    switch (dBUpdate.dbProvider.ToLower())
+    {
+      case "mongo":
+        _dbService.UpdateDataFromMongoDB(dBUpdate.connectionId, dBUpdate.property, dBUpdate.selecteddbId, dBUpdate.itemId, _dbHub);
+        break;
+      default:
+        break;
+    }
+    return new { Success = true };
+  }
+
+  [HttpGet("dbconnector")]
+  public object GetDBData()
+  {
+    string connectionId = base.Request.Query["connectionId"];
+    //string externalId = base.Request.Query["externalId"];
+    string selecteddbId = base.Request.Query["selecteddbId"];
+    string dbProvider = base.Request.Query["dbProvider"];
+    //string projectId = base.Request.Query["projectId"];
+    string itemId = base.Request.Query["itemId"];
+
+
+    switch (dbProvider.ToLower())
+    {
+      case "mongo":
+        _dbService.ExtractDataFromMongoDB(connectionId, selecteddbId, itemId, _dbHub);
+        break;
+      default:
+        break;
+    }
+
+    return new { Success = true };
+  }
+
+}
+
+public class DBUpdate
+{
+  public Property property { get; set; }
+  public string connectionId { get; set; }
+  public string dbProvider { get; set; }
+  public string selecteddbId { get; set; }
+  public string itemId { get; set; }
+}
+
+public class Property
+{
+  public string category { get; set; }
+  public string name { get; set; }
+  public string value { get; set; }
+}
---
+++ Controllers/HubsController.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HubsController : ControllerBase
+{
+  private readonly ILogger<HubsController> _logger;
+  private readonly APSService _APSService;
+
+  public HubsController(ILogger<HubsController> logger, APSService APSService)
+  {
+    _logger = logger;
+    _APSService = APSService;
+  }
+
+  [HttpGet()]
+  public async Task<ActionResult<string>> ListHubs()
+  {
+    var tokens = await AuthController.PrepareTokens(Request, Response, _APSService);
+    if (tokens == null)
+    {
+      return Unauthorized();
+    }
+    var hubs = await _APSService.GetHubs(tokens);
+    return JsonConvert.SerializeObject(hubs);
+  }
+
+  [HttpGet("{hub}/projects")]
+  public async Task<ActionResult<string>> ListProjects(string hub)
+  {
+    var tokens = await AuthController.PrepareTokens(Request, Response, _APSService);
+    if (tokens == null)
+    {
+      return Unauthorized();
+    }
+    var projects = await _APSService.GetProjects(hub, tokens);
+    return JsonConvert.SerializeObject(projects);
+  }
+
+  [HttpGet("{hub}/projects/{project}/contents")]
+  public async Task<ActionResult<string>> ListItems(string hub, string project, [FromQuery] string? folder_id)
+  {
+    var tokens = await AuthController.PrepareTokens(Request, Response, _APSService);
+    if (tokens == null)
+    {
+      return Unauthorized();
+    }
+    var contents = await _APSService.GetContents(hub, project, folder_id, tokens);
+    return JsonConvert.SerializeObject(contents);
+  }
+
+  [HttpGet("{hub}/projects/{project}/contents/{item}/versions")]
+  public async Task<ActionResult<string>> ListVersions(string hub, string project, string item)
+  {
+    var tokens = await AuthController.PrepareTokens(Request, Response, _APSService);
+    if (tokens == null)
+    {
+      return Unauthorized();
+    }
+    var versions = await _APSService.GetVersions(hub, project, item, tokens);
+    return JsonConvert.SerializeObject(versions);
+  }
+}
